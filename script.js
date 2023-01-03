53bfd12e8abf48de8e65ef94537fda8f18169b77
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-01-03 20:29:03 -0300

    Add more functions and refactor

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const containerDivs = document.querySelector(".grid-container");
+const containerDivs = document.querySelector("#grid-container");
+let divItem;
 
-function createDivs(numOfDivs) {
+function createGridContainer() {
+  containerDivs.style.display = "grid";
+  containerDivs.style.width = "500px";
+  containerDivs.style.height = "500px";
+}
+
+function createGrid(numOfDivs) {
+  containerDivs.style.gridTemplateColumns = `repeat(${numOfDivs}, auto)`;
   let multiplyDivs = numOfDivs * numOfDivs;
   for (let i = 0; i < multiplyDivs; i++) {
     divItem = document.createElement("div");
@@ -12,26 +20,50 @@ function createDivs(numOfDivs) {
 function changeDivColor(color) {
   let gridDivs = containerDivs.querySelectorAll("div");
 
-  gridDivs.forEach(target => {
-    target.addEventListener("mouseover", function(e) {
+  gridDivs.forEach((target) => {
+    target.addEventListener("mouseover", function (e) {
       target.style.backgroundColor = color;
     });
   });
 }
 
-function clearContainerDivs() {
-  containerDivs.innerHTML = "";
-  createDivs(16);
-  changeDivColor("red");
+function randomRgb() {
+  let randomNum = Math.floor(Math.random() * 255);
+  return randomNum;
 }
 
-let clearContainerGrid = document.querySelector(".clear-btn");
-clearContainerGrid.addEventListener("click", clearContainerDivs);
+function randomColor() {
+  let gridDivs = containerDivs.querySelectorAll("div");
 
+  gridDivs.forEach((target) => {
+    target.addEventListener("mouseover", function (e) {
+      target.style.backgroundColor = `rgb(${randomRgb()}, ${randomRgb()}, ${randomRgb()})`;
+    });
+  });
+}
 
-createDivs(16);
-changeDivColor("red");
+function clearGrid() {
+  containerDivs.replaceChildren(divItem, divItem);
+}
 
+function clearContainerDivs() {
+  let gridDivs = containerDivs.querySelectorAll("div");
 
+  gridDivs.forEach((target) => {
+    target.style.backgroundColor = "white";
+  });
+}
 
+let clearContainerGridButton = document.querySelector(".clear-btn");
+clearContainerGridButton.addEventListener("click", clearContainerDivs);
 
+let raibowColorDivButton = document.querySelector(".rainbow-btn");
+raibowColorDivButton.addEventListener("click", randomColor);
+
+function showGrid() {
+  createGridContainer();
+  createGrid(16);
+  changeDivColor("black");
+}
+
+showGrid();
